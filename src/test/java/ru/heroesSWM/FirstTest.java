6b834c9cf41636a1bd52674b273ef0173a9a6fef
Chinger <Chinger1104@yandex.ru>
2019-10-26 17:15:29 +0300

    добавлены логин с паролем и клик по кнопке входа.

--- src/test/java/ru/heroesSWM/FirstTest.java
+++ src/test/java/ru/heroesSWM/FirstTest.java
@@ -1,6 +1,7 @@
 package ru.heroesSWM;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
@@ -10,6 +11,8 @@ public class FirstTest {
         System.setProperty("webdriver.chrome.driver", "/Users/tatanafedorova/IdeaProjects/GVDRulet/src/test/resources/chromedriver");
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://www.heroeswm.ru/");
-
+        driver.findElement(By.xpath("//input[@name='login']")).sendKeys("Testerovchik");
+        driver.findElement(By.xpath("//input[@name='pass']")).sendKeys("Cxld3yoev7");
+        driver.findElement(By.xpath("//input[@title='Войти в игру!']")).click();
     }
 }
